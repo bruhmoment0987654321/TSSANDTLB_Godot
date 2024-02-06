3e3bc862b2234f0bffb574d09c8c9a479609b104
bruhmoment0987654321 <lopeg452@hsd.k12.or.us>
2024-02-06 01:07:43 -0800

    starting to make the dash

---
+++ Player/Slime_Player/Scripts/slime_player.gd
@@ -0,0 +1,114 @@
+extends CharacterBody2D
+@export_group("Movement Data")
+@export var movement_data : PlayerMovementData
+
+#putting other nodes in variables 
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
+@onready var dash_timer = $DashTimer
+@onready var dash_particle = $DashParticles
+
+#slime player state
+enum STATE{NORMAL,DASH,DEAD}
+var player_state = STATE.NORMAL
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#dash variables
+@export_group("Dashing") 
+#having the player dash or not?
+@export var player_dash = true
+@export var dash_speed = 1000
+@export var dash_length = 0.2
+@export var max_dash_amount = 2
+var dash_object = preload("res://Player/Slime_Player/Scenes/dash_sprite.tscn")
+var dash_amount = max_dash_amount
+var is_dashing = false #check if we are dashing
+var can_dash = true 
+var dash_direction = Vector2() #get direciton we'll dash in
+
+func _ready():
+	dash_timer.timeout.connect(dash_timer_timeout)
+	pass
+
+func dash_timer_timeout():
+	is_dashing = false
+
+func _physics_process(delta):
+	if player_state == STATE.NORMAL:
+		apply_gravity(delta)
+		handle_jump()
+		start_dashing()
+		var input_axis = Input.get_axis("left", "right")
+		handle_acceleration(input_axis,delta)
+		apply_friction(input_axis,delta)
+		apply_air_resistance(input_axis,delta)
+		update_animation(input_axis)
+		var was_on_floor = is_on_floor()
+		move_and_slide()
+		var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+		if just_left_ledge:
+			coyote_jump_timer.start()
+			
+	if player_state == STATE.DASH:
+		pass
+
+func apply_gravity(delta):
+	if not is_on_floor():
+		velocity.y += gravity* movement_data.gravity_scale * delta
+
+func handle_jump():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = movement_data.jump_velocity
+	if not is_on_floor():
+		if Input.is_action_just_released("jump") and velocity.y < movement_data.jump_velocity/3:
+			velocity.y = movement_data.jump_velocity/3
+
+func start_dashing():
+	if Input.is_action_just_pressed("dash") and can_dash and dash_amount > 0:
+		player_state = STATE.DASH
+		dash_amount -= 1
+		is_dashing = true
+		dash_direction = get_direction_from_input()
+		dash_timer.start(dash_length)
+
+func handle_acceleration(input_axis,delta):
+	var _walk_multiplied = 1
+	if movement_data.running:
+		if Input.is_action_pressed("run"):
+			_walk_multiplied = movement_data.run_multiplier
+	if input_axis: #if direction != 0
+		velocity.x = move_toward(velocity.x,movement_data.hspeed*input_axis*_walk_multiplied,movement_data.acceleration*delta)
+
+func apply_friction(input_axis,delta):
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.friction*delta)
+
+func apply_air_resistance(input_axis,delta):
+	if input_axis == 0 and not is_on_floor():
+		velocity.x = move_toward(velocity.x,0,movement_data.air_resistance*delta)
+
+func update_animation(input_axis):
+	if input_axis :
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("walk")
+	else:
+		animated_sprite_2d.play("idle")
+	if not is_on_floor():
+		if velocity.y < 0:
+			animated_sprite_2d.play("jump")
+		else:
+			animated_sprite_2d.play("fall")
+
+func get_direction_from_input():
+	var move_dir = Vector2()
+	move_dir.x = Input.get_action_strength("left")+Input.get_action_strength("right")
+	move_dir.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	move_dir = move_dir.limit_length(1)
+	#check if no buttons are pressed... if they aren't, then you still get to move  :]  towards the position you're facing
+	if move_dir == Vector2(0,0):
+		if animated_sprite_2d.flip_h:
+			move_dir.x = -1
+		else:
+			move_dir = 1
+	return move_dir * dash_speed
